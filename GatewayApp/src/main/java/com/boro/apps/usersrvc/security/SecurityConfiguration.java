f278e273a3baa8176b78ad3bd84d1d76b99618af
borovinskiy_me <m.borovinskiy@mail.ru>
2026-04-18 12:33:58 +0300

    task_01 added path props

--- GatewayApp/src/main/java/com/boro/apps/usersrvc/security/SecurityConfiguration.java
+++ GatewayApp/src/main/java/com/boro/apps/usersrvc/security/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.boro.apps.usersrvc.security;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
@@ -8,8 +9,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -19,21 +18,23 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 @EnableR2dbcRepositories
+@RequiredArgsConstructor
 public class SecurityConfiguration  {
+
+    private final SecurityPathsProperties paths;
 
     @Bean
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         return http
                 .authorizeExchange((authorize) -> authorize
-                        .pathMatchers("/login", "/signup", "/logout", "/static/public/css/*", "/GatewayApp/src/main/resources/static/public/css/*", "/users/static/public/css/*").permitAll()
-                        .pathMatchers(HttpMethod.POST, "/users/v1/users").permitAll()
-                        .pathMatchers(HttpMethod.GET, "/users/v1/users/*", "/logout", "/", "/users/v1/user_authorities", "/selfinfo").authenticated()
-                        .pathMatchers(HttpMethod.GET, "/users/v1/users").hasAnyAuthority("ROLE_ADMIN")
-                        .pathMatchers(HttpMethod.GET, "/finmonitoring/v1/*", "/finmonitoring/v1/*/*", "/expertises", "/found_res", "/save_res", "/upd_res",
-                                "/GatewayApp/src/main/resources/templates/*", "/ExpertiseMonitoringService/src/main/resources/templates/*").hasAnyAuthority("ROLE_MONITORING_USER")
-                        .pathMatchers(HttpMethod.POST, "/found_res", "/finmonitoring/v1/*", "/finmonitoring/v1/*/*", "/index").hasAnyAuthority("ROLE_MONITORING_USER")
-                        .pathMatchers(HttpMethod.PUT, "/found_res", "/finmonitoring/v1/*", "/finmonitoring/v1/*/*").hasAnyAuthority("ROLE_MONITORING_USER")
-                        .pathMatchers(HttpMethod.DELETE, "/found_res", "/finmonitoring/v1/*", "/finmonitoring/v1/*/*").hasAnyAuthority("ROLE_MONITORING_USER")
+                        .pathMatchers(paths.getPublicGet().toArray(String[]::new)).permitAll()
+                        .pathMatchers(HttpMethod.POST, paths.getPublicPost().toArray(String[]::new)).permitAll()
+                        .pathMatchers(HttpMethod.GET, paths.getAuthenticatedGet().toArray(String[]::new)).authenticated()
+                        .pathMatchers(HttpMethod.GET, paths.getAdminGet().toArray(String[]::new)).hasAnyAuthority("ROLE_ADMIN")
+                        .pathMatchers(HttpMethod.GET, paths.getMonitoringGet().toArray(String[]::new)).hasAnyAuthority("ROLE_MONITORING_USER")
+                        .pathMatchers(HttpMethod.POST, paths.getMonitoringPost().toArray(String[]::new)).hasAnyAuthority("ROLE_MONITORING_USER")
+                        .pathMatchers(HttpMethod.PUT, paths.getMonitoringPost().toArray(String[]::new)).hasAnyAuthority("ROLE_MONITORING_USER")
+                        .pathMatchers(HttpMethod.DELETE, paths.getMonitoringPost().toArray(String[]::new)).hasAnyAuthority("ROLE_MONITORING_USER")
                 )
                 .httpBasic(withDefaults())
                 .formLogin((form) -> form
